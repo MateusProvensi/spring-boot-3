00e7e1862de4aa5e392cd3c2a2ac4f9197c9c2ad
MateusProvensi <mateussprovensidev@outlook.com>
2023-03-01 20:56:55 -0300

    Uploaded Config Files

--- api-spring-3/src/main/java/br/com/provensi/controllers/FileController.java
+++ api-spring-3/src/main/java/br/com/provensi/controllers/FileController.java
@@ -1,17 +1,14 @@
 package br.com.provensi.controllers;
 
-import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +18,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.provensi.data.vo.v1.UploadFileResponseVO;
-import br.com.provensi.exceptions.MyFileNotFoundException;
 import br.com.provensi.services.FileStorageService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
--- api-spring-3/src/main/java/br/com/provensi/services/FileStorageService.java
+++ api-spring-3/src/main/java/br/com/provensi/services/FileStorageService.java
@@ -22,50 +22,46 @@ public class FileStorageService {
 	private final Path fileStorageLocation;
 
 	@Autowired
-	public FileStorageService(
-			FileStorageConfig fileStorageConfig) {
+	public FileStorageService(FileStorageConfig fileStorageConfig) {
 		Path path = Paths.get(fileStorageConfig.getUploadDir()).toAbsolutePath().normalize();
-		
+
 		this.fileStorageLocation = path;
-		
+
 		try {
 			Files.createDirectories(this.fileStorageLocation);
 		} catch (Exception e) {
-			throw new FileStorageException("Could not create the directory where the uploaded files will be stored!", e);
+			throw new FileStorageException(
+					"Could not create the directory where the uploaded files will be stored!",
+					e);
 		}
 	}
-	
+
 	public String storeFile(MultipartFile file) {
 		String filename = StringUtils.cleanPath(file.getOriginalFilename());
-		
 		try {
-			
+			// Filename..txt
 			if (filename.contains("..")) {
 				throw new FileStorageException("Sorry! Filename contains invalid path sequence " + filename);
 			}
-			
 			Path targetLocation = this.fileStorageLocation.resolve(filename);
-			
 			Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
-			
 			return filename;
 		} catch (Exception e) {
-			throw new FileStorageException("Could not store file " + filename + ". Please, try again!", e);
+			throw new FileStorageException("Could not store file " + filename + ". Please try again!", e);
 		}
 	}
-	
+
 	public Resource loadFileAsResource(String filename) {
 		try {
 			Path filePath = this.fileStorageLocation.resolve(filename).normalize();
-			
 			Resource resource = new UrlResource(filePath.toUri());
-			
-			if (!resource.exists()) {
-				throw new MyFileNotFoundException("File Not Found: " + filename);
-			}
-			return resource;
+			if (resource.exists())
+				return resource;
+			else
+				throw new MyFileNotFoundException("File not found");
 		} catch (Exception e) {
-			throw new MyFileNotFoundException("File Not Found: " + filename, e);
+			throw new MyFileNotFoundException("File not found" + filename, e);
 		}
 	}
+
 }
